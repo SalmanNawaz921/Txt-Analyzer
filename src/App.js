c44c0424ce652efec4b6b012ad7727af8718813e
Muhammad <salmannawaz008@gmail.com>
2023-08-24 23:03:18 +0500

    Txt-Analyzer

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import "./App.css";
 import { Alert } from "./components/Alert";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-import About from "./components/About";
 import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
   const [mode, setMode] = useState("light");
   const [alert, setAlert] = useState(null);
@@ -32,22 +30,9 @@ function App() {
   };
   return (
     <>
-      <Router>
-        <Navbar title="Salman" mode={mode} toggleMode={changeMode} />
-        <Alert alert={alert} />
-        <Switch>
-          <Route exact path="/about">
-            <About />
-          </Route>
-          <Route exact path="/">
-            <TextForm
-              title="Enter Your Text "
-              mode={mode}
-              showAlert={showAlert}
-            />
-          </Route>
-        </Switch>
-      </Router>
+      <Navbar title="Salman" mode={mode} toggleMode={changeMode} />
+      <Alert alert={alert} />
+      <TextForm title="Enter Your Text " mode={mode} showAlert={showAlert} />
     </>
   );
 }
---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+      {/* <Navbar title="Salman" mode={mode} toggleMode={changeMode} /> */}
